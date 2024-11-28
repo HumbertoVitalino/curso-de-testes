99b2cb8b51aa5e409974c5c15199fba44dfef52c
Humberto Vitalino <humbertodark@outlook.com>
2024-11-28 20:25:45 -0300

    creating test checking if rote is not valid

--- testes/UnitTests/OfertaViagemTeste.cs
+++ testes/UnitTests/OfertaViagemTeste.cs
@@ -16,4 +16,20 @@ public class OfertaViagemTeste
 
         Assert.Equal(validacao, oferta.EhValido);
     }
+
+    [Fact]
+    public void TestingIfTheOfferIsNotValid()
+    {
+        //arrange
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double preco = 100.0;
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
 }
