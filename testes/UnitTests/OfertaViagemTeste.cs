ec54b64da2a312285daaddedf02df0efd612a539
Humberto Vitalino <humbertodark@outlook.com>
2024-11-28 20:51:32 -0300

    Update OfertaViagemTeste.cs

--- testes/UnitTests/OfertaViagemTeste.cs
+++ testes/UnitTests/OfertaViagemTeste.cs
@@ -51,4 +51,20 @@ public class OfertaViagemTeste
         Assert.Contains("Erro: Data de ida não pode ser maior que a data de volta.", oferta.Erros.Sumario);
         Assert.False(oferta.EhValido);
     }
+
+    [Fact]
+    public void TestingIfTheOfferIsNotValidWithPrice()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double preco = 0;
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
 }
