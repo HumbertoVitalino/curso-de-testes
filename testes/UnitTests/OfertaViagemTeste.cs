2e6220397b593cd9dab72695458fe3d9b1bac750
Humberto Vitalino <humbertodark@outlook.com>
2024-11-28 20:33:20 -0300

    creating test to check if period is valid

--- testes/UnitTests/OfertaViagemTeste.cs
+++ testes/UnitTests/OfertaViagemTeste.cs
@@ -7,13 +7,16 @@ public class OfertaViagemTeste
     [Fact]
     public void TestingIfTheOfferIsValid()
     {
+        //arrange
         Rota rota = new Rota("OrigemTeste", "DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
         double preco = 100.0;
         var validacao = true;
-
+        
+        //act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+        //assert
         Assert.Equal(validacao, oferta.EhValido);
     }
 
@@ -30,6 +33,22 @@ public class OfertaViagemTeste
 
         //assert
         Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
+
+    [Fact]
+    public void TestingIfTheOfferIsNotValidWithPeriod()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 10), new DateTime(2024, 2, 5));
+        double preco = 100.0;
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Contains("Erro: Data de ida não pode ser maior que a data de volta.", oferta.Erros.Sumario);
         Assert.False(oferta.EhValido);
     }
 }
