7c1e38b67a16b104a42afa7c032fdee68b9fe8e9
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2024-12-01 23:32:28 -0300

    feat: route test

---
+++ testes/UnitTests/RotaTeste.cs
@@ -0,0 +1,24 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests;
+
+public class RotaTeste
+{
+    [Fact]
+    public void TestingIfRoteIsValid()
+    {
+        //arrange
+        Rota rota = new Rota("São Paulo", "Rio de Janeiro");
+
+        //act
+        rota.Validar();
+
+        //assert
+        Assert.True(rota.EhValido);
+    }
+}
