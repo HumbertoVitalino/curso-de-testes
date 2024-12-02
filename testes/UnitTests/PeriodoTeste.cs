a282c2fc0b2e8e3f48ab5c67ea4502a14fc37f4d
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2024-12-01 23:44:33 -0300

    feat: testingifperiodisvalid

---
+++ testes/UnitTests/PeriodoTeste.cs
@@ -0,0 +1,26 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests;
+
+public class PeriodoTeste
+{
+    [Fact]
+    public void TestingIfPeriodIsValid()
+    {
+        //arrange
+        Periodo periodo = new Periodo(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+
+        //act
+        periodo.Validar();
+
+        //assert
+        Assert.True(periodo.EhValido);
+    }
+
+    
+}
