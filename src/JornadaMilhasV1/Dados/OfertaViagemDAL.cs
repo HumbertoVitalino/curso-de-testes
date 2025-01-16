973dee84f502ef8042c0cadfaed9334eb164d2e5
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-16 07:40:38 -0300

    Update OfertaViagemDAL.cs

--- src/JornadaMilhasV1/Dados/OfertaViagemDAL.cs
+++ src/JornadaMilhasV1/Dados/OfertaViagemDAL.cs
@@ -1,10 +1,4 @@
 ﻿using JornadaMilhasV1.Modelos;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JornadaMilhas.Dados;
 public class OfertaViagemDAL
