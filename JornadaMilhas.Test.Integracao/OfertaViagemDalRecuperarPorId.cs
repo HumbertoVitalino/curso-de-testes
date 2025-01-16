e17613c12f90a193ddeb0ee80e855804af95ff2f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-16 07:40:25 -0300

    test: recuperarTodos

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalRecuperarPorId.cs
@@ -0,0 +1,29 @@
+﻿using JornadaMilhas.Dados;
+using JornadaMilhas.Test.Integracao.Fixture;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao;
+
+[Collection(nameof(ContextoCollection))]
+public class OfertaViagemDalRecuperarPorId
+{
+    private readonly JornadaMilhasContext _context;
+    public OfertaViagemDalRecuperarPorId(ITestOutputHelper output, ContextoFixture fix)
+    {
+        _context = fix.Context;
+        output.WriteLine(_context.GetHashCode().ToString());
+    }
+
+    [Fact]
+    public void RetornaNuloQuandoInexistente()
+    {
+        //arrange
+        var dal = new OfertaViagemDAL(_context);
+
+        //act
+        var ofertaRecuperada = dal.RecuperarPorId(-2);
+
+        //assert
+        Assert.Null(ofertaRecuperada);
+    }
+}
