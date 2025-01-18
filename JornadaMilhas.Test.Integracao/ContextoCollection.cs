223e47e6d0eece0c327e4ad46930b0144d4b1431
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-18 07:32:36 -0300

    update

--- JornadaMilhas.Test.Integracao/ContextoCollection.cs
+++
@@ -1,8 +0,0 @@
-﻿using JornadaMilhas.Test.Integracao.Fixture;
-
-namespace JornadaMilhas.Test.Integracao;
-
-[CollectionDefinition(nameof(ContextoCollection))]
-public class ContextoCollection : ICollectionFixture<ContextoFixture>
-{
-}
